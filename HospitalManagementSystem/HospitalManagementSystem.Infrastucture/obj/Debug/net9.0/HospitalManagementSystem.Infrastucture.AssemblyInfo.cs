6ae62b1e5766a0f091e3e98418e9d2c67b5fce3a
buigiau <sacsigiau@gmail.com>
2024-12-10 17:50:03 +0700

    commit local changes before merging

--- HospitalManagementSystem/HospitalManagementSystem.Infrastucture/obj/Debug/net9.0/HospitalManagementSystem.Infrastucture.AssemblyInfo.cs
+++ HospitalManagementSystem/HospitalManagementSystem.Infrastucture/obj/Debug/net9.0/HospitalManagementSystem.Infrastucture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem.Infrastucture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0057ab83d4cf77e3dc63127b06ec58901caf97a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem.Infrastucture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem.Infrastucture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
