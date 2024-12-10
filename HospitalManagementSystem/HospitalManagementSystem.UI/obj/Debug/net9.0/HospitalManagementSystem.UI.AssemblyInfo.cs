31f1106555573501111a34666c21e8613b6c7e10
buigiau <sacsigiau@gmail.com>
2024-12-10 17:04:16 +0700

    loginview

--- HospitalManagementSystem/HospitalManagementSystem.UI/obj/Debug/net9.0/HospitalManagementSystem.UI.AssemblyInfo.cs
+++ HospitalManagementSystem/HospitalManagementSystem.UI/obj/Debug/net9.0/HospitalManagementSystem.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0057ab83d4cf77e3dc63127b06ec58901caf97a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
